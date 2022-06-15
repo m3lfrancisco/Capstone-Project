07523e6e65933ee0061725fe0e235d6eb5592b55
mfrancisco <francisco.melanie@gmail.com>
2022-06-15 13:48:12 -0700

    add orders.js to controllers/api

---
+++ controllers/api/orders.js
@@ -0,0 +1,42 @@
+const Order = require('../../models/order');
+
+module.exports = {
+    cart,
+    addToCart,
+    setItemQtyInCart,
+    checkout,
+    history
+};
+
+// A cart is the unpaid order for a user
+async function cart(req, res) {
+    const cart = await Order.getCart(req.user._id);
+    res.json(cart);
+}
+
+// Add a wine to the cart
+async function addToCart(req, res) {
+    const cart = await Order.getCart(req.user._id);
+    await cart.addItemToCart(req.params.id);
+    res.json(cart);
+}
+
+// Updates a wine's qty in the cart
+async function setItemQtyInCart(req, res) {
+    let cart = await Order.getCart(req.user._id);
+    await cart.setItemQty(req.body.itemId, req.body.newQty);
+    res.json(cart);
+}
+
+// Update the cart's isPaid property to true
+async function checkout(req, res) {
+    const cart = await Order.getCart(req.user._id);
+    cart.isPaid = true;
+    await cart.save();
+    res.json(cart);
+}
+
+async function history(req, res) {
+    const orders = await Order.find({ user: req.user._id, isPaid: true}).sort('-updatedAt').exec();
+    res.json(orders);
+}
