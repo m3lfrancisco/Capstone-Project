9f488af142c0203bfa5a5c680431f34960bcf66c
mfrancisco <francisco.melanie@gmail.com>
2022-06-15 09:33:57 -0700

    add wine model and wineSchema

---
+++ models/wineSchema.js
@@ -0,0 +1,14 @@
+const Schema = require('mongoose').Schema;
+
+const wineSchema = new Schema({
+    winery: { type: String, required: true },
+    name: { type: String, required: true },
+    location: { type: String, required: true },
+    image: {  type: String },
+    category: { type: Schema.Types.ObjectId, ref: 'Category'},
+    price: { type: Number, required: true, default: 0 }
+}, {
+    timestamps: true
+});
+
+module.exports = wineSchema;
