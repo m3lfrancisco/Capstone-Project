9a39cb44fe805a93ebcfd8bd6f06a87f5d76a720
mfrancisco <francisco.melanie@gmail.com>
2022-06-15 09:41:42 -0700

    change root.render to ReactDOM.render in index.js

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ import {BrowserRouter as Router} from 'react-router-dom';
 import './index.css';
 import App from './pages/App/App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
     <React.StrictMode>
         <Router><App/></Router>
-    </React.StrictMode>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
