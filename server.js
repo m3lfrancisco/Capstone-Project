be7a70de2ef858084e99058b58e64cebf4452ed8
mfrancisco <francisco.melanie@gmail.com>
2022-06-15 09:59:12 -0700

    add .env file and api routes with ensureLoggedIn to server.js

--- server.js
+++ server.js
@@ -2,13 +2,41 @@ const express = require('express');
 const path = require('path');
 const favicon = require('serve-favicon');
 const logger = require('morgan');
+
+// Always require near the top
 require('dotenv').config();
+// Connect to the server
+// Make sure that dotenv is already required
 require('./config/database');
 
 const app = express();
 
 app.use(logger('dev'));
-app.use(express.json())
+// body parser middleware - adds properties to req.body
+app.use(express.json());
+
+// Configure both serve-favicon & static
+// middleware to server from the 'build' folder
+app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
+app.use(express.static(path.join(__dirname, 'build')));
+
+// Middleware to verify token and assign user object of payload to req.user.
+// Be sure to mount before routes
+app.use(require('./config/checkToken'));
+
+// Put API routes here, before the "catch all" route
+app.use('/api/users', require('./routes/api/users'));
+
+// Protect the api routes below from anonymous users
+const ensureLoggedIn = require('./config/ensureLoggedIn');
+app.use('/api/items', ensureLoggedIn, require('./routes/api/items'));
+app.use('/api/orders', ensureLoggedIn, require('./routes/api/orders'));
+
+// The following "catch all" route (note the *) is necessary
+// to return the index.html on all non-AJAX requests
+app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 // Configure to use port 3001 instead of 3000 during
 // development to avoid collision with React's dev server
@@ -16,17 +44,4 @@ const port = process.env.PORT || 3001;
 
 app.listen(port, function() {
     console.log(`Express app running on port ${port}`)
-});
-
-app.use(express.static(path.join(__dirname, 'build')));
-
-app.use(require('./config/checkToken'));
-
-// Put API routes here, before the "catch all" route
-app.use('/api/users', require('./routes/api/users'));
-
-// The following "catch all" route (note the *) is necessary
-// to return the index.html on all non-AJAX requests
-app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
