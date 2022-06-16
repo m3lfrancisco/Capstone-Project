f7e2f93c55e7adadd6db061d6c4004fe68510445
mfrancisco <francisco.melanie@gmail.com>
2022-06-15 22:53:56 -0700

    code OrderHistoryPage

--- src/pages/OrderHistoryPage/OrderHistoryPage.jsx
+++ src/pages/OrderHistoryPage/OrderHistoryPage.jsx
@@ -1,17 +1,47 @@
-import { checkToken } from "../../utilities/users-service";
+// import { checkToken } from "../../utilities/users-service";
+import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import OrderList from "../../components/OrderList/OrderList";
+import UserLogOut from '../../components/UserLogOut/UserLogOut';
+import * as ordersAPI from '../../utilities/orders-api';
+import './OrderHistoryPage.css';
 
-export default function OrderHistoryPage() {
 
-    async function handleCheckToken() {
-        // alert("clicked");
-        const expDate = await checkToken();
-        console.log(expDate);
+export default function OrderHistoryPage({ user,setUser }) {
+    const [orders, setOrders] = useState([]);
+    const [activeOrder, setActiveOrder] = useState(null);
+    
+    useEffect(function() {
+        // Get previous orders (paid)
+        async function getOrders() {
+            const orders = await ordersAPI.getOrderHistory();
+            setOrders(orders);
+            // If no previous orders, activeOrder will be set to null
+            setActiveOrder(orders[0] || null);
+        }
+        getOrders();
+    }, []);
+
+    function handleSelectOrder(order) {
+        setActiveOrder(order);
     }
 
+    // async function handleCheckToken() {
+    //     // alert("clicked");
+    //     const expDate = await checkToken();
+    //     console.log(expDate);
+    // }
+
     return (
-        <>
-            <h1>OrderHistoryPage</h1>
-            <button onClick={handleCheckToken}>Check When My Login Expires</button>
-        </>
+        <main className="OrderHistoryPage">
+            <h1>Order History</h1>
+            <Link to="/orders/new" className="button btn-sm">NEW ORDER</Link>
+            <UserLogOut user={user} setUser={setUser} />
+            <OrderList 
+                orders={orders} 
+                activeOrder={activeOrder}
+                handleSelectOrder={handleSelectOrder}
+            />
+        </main>
     );
 }
