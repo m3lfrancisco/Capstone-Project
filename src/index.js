862edcf3447ffb56cdaf9bed86c722e864b90734
mfrancisco <francisco.melanie@gmail.com>
2022-06-15 22:51:37 -0700

    try to fix error in console and switch the way ReactDOM.render is written

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import {BrowserRouter as Router} from 'react-router-dom';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 import App from './pages/App/App';
 
