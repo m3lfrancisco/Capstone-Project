a9e65c9d0db477896a57a5704fd10b3553c1e5c8
mfrancisco <francisco.melanie@gmail.com>
2022-06-21 05:25:37 -0700

    use react bootstrap card for WineListItem and add css file

--- src/components/WineListItem/WineListItem.jsx
+++ src/components/WineListItem/WineListItem.jsx
@@ -1,17 +1,29 @@
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Card from 'react-bootstrap/Card';
+import './WineListItem.css';
+
 export default function WineListItem({ wineItem, handleAddToOrder }) {
     return (
-        <div className="WineListItem">
-            <img src={wineItem.image} alt=""/>
-            <div className="winery">{wineItem.winery}</div>
-            <div className="location">{wineItem.location}</div>
-            <div className="name">{wineItem.name}</div>
-            <div className="buy">
-                <span>${wineItem.price.toFixed(2)}</span>
-                <button className="btn-sm" onClick={() => 
-                    handleAddToOrder(wineItem._id)}>
-                    ADD
-                </button>
-            </div>
-        </div>
+        <Container className="WineContainer">
+            <Row>
+                <Col xs={6} md={4}>
+                    <Card className="WineCard" style={{ width: "18rem" }}>
+                        <Card.Img className="wine-img" variant="top" src={wineItem.image} />
+                        <Card.Body>
+                            <Card.Title>{wineItem.name}</Card.Title>
+                            <Card.Subtitle className="mb-2">{wineItem.winery}</Card.Subtitle>
+                            <Card.Text>{wineItem.location}</Card.Text>
+                            <Card.Text>${wineItem.price.toFixed(2)}</Card.Text>
+                            <button className="btn-sm" onClick={() => 
+                                handleAddToOrder(wineItem._id)}>
+                                ADD
+                            </button>
+                        </Card.Body>
+                    </Card>
+                </Col>
+            </Row>
+        </Container>
     );
 }
