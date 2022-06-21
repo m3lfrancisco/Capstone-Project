ee4a9b3796a21f913e8e0db8e79cd8bc0584e38e
mfrancisco <francisco.melanie@gmail.com>
2022-06-21 11:34:04 -0700

    remove commented out code

--- src/utilities/wines-api.js
+++ src/utilities/wines-api.js
@@ -5,7 +5,3 @@ const BASE_URL = '/api/wines';
 export function getAll() {
     return sendRequest(BASE_URL);
 }
-
-// export function getById(id) {
-//     return sendRequest(`${BASE_URL}/${id}`);
-// }
