ccf189524efc02df16ee6fae6cacc74ac922726b
mfrancisco <francisco.melanie@gmail.com>
2022-06-21 05:50:48 -0700

    add assets folder with file for AuthPage video

--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import SignUpForm from '../../components/SignUpForm/SignUpForm';
 import LoginForm from '../../components/LoginForm/LoginForm';
+import videoBg from '../../assets/Vines.mp4';
 import './AuthPage.css';
 
 export default function AuthPage({ setUser }) {
@@ -14,7 +15,7 @@ export default function AuthPage({ setUser }) {
                 <div className="welcome-msg">
                     <h1>Welcome To Vintage Vintner</h1>
                 </div>
-                <video autoPlay muted loop src="https://vod-progressive.akamaized.net/exp=1655823090~acl=%2Fvimeo-prod-skyfire-std-us%2F01%2F2532%2F15%2F387662274%2F1635192732.mp4~hmac=70cecedb77b7d5285897e12222cb62652ba7d800f3ab72aa2f4355f25b320ebd/vimeo-prod-skyfire-std-us/01/2532/15/387662274/1635192732.mp4?filename=Vines+-+31527.mp4" />
+                <video autoPlay muted loop src={videoBg} />
             </div>
         </main>
     );
