ac2506c8cdbd375627ade91161d03bccdf6395c7
mfrancisco <francisco.melanie@gmail.com>
2022-06-21 05:23:00 -0700

    change navbar to react bootstrap and add NavBar.css

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,5 +1,9 @@
-import {Link} from "react-router-dom";
 import * as userService from '../../utilities/users-service';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Offcanvas from 'react-bootstrap/Offcanvas'
+import './NavBar.css';
 
 export default function NavBar(props) {
 
@@ -9,14 +13,33 @@ export default function NavBar(props) {
     }
 
     return (
-        <nav>
-            <Link to="/orders">Order History</Link>
-            &nbsp; | &nbsp;
-            <Link to="/orders/new">New Order</Link>
-            &nbsp; | &nbsp;
-            <span>Welcome {props.user.name}</span>
-            &nbsp; | &nbsp;
-            <Link to="" onClick={handleLogOut}>Logout</Link>
-        </nav>
+        <>
+            <Navbar expand="lg" variant="dark" className="mb-3 NavBar" sticky="top">
+                <Container fluid>
+                    <Navbar.Brand className="NavbarBrandText">Vintage Vintner</Navbar.Brand>
+                    <Navbar.Text>Welcome {props.user.name}</Navbar.Text>
+                    <Navbar.Toggle aria-controls="offcanvasNavbar-expand-lg" />
+                    <Navbar.Offcanvas
+                        id="offcanvasNavbar-expand-lg"
+                        aria-labelledby="offcanvasNavbar-expand-lg"
+                        placement="end"
+                    >
+                        <Offcanvas.Header closeButton>
+                            <Offcanvas.Title id="offcanvasNavbar-expand-lg" className="drawer-title">
+                                Vintage Vintner
+                            </Offcanvas.Title>
+                        </Offcanvas.Header>
+                        <Offcanvas.Body>
+                            <Nav className="justify-content-end flex-grow-1 pe-3">
+                                <Nav.Link href="/orders">Order History</Nav.Link>
+                                <Nav.Link href="/orders/new">New Order</Nav.Link>
+                                <Nav.Link href="/cart">Cart</Nav.Link>
+                                <Nav.Link href="" onClick={handleLogOut}>Logout</Nav.Link>
+                            </Nav>
+                        </Offcanvas.Body>
+                    </Navbar.Offcanvas>
+                </Container>
+            </Navbar>
+        </>
     );
 }
