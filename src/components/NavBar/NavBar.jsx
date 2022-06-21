b97cf753e468a9fd5c4b6f0893e48f448730521c
mfrancisco <francisco.melanie@gmail.com>
2022-06-21 10:01:24 -0700

    delete NavBar.css file and transfer styles to index.css

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -3,7 +3,6 @@ import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Offcanvas from 'react-bootstrap/Offcanvas'
-// import './NavBar.css';
 
 export default function NavBar(props) {
 
