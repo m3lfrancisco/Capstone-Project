6cd49a426f366148facb947a3392c65eba16ab9e
mfrancisco <francisco.melanie@gmail.com>
2022-06-21 05:33:10 -0700

    add CartDetailPage component and route to App.jsx

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,4 +1,3 @@
-import './App.css';
 import { useState } from "react";
 import { Route, Switch, Redirect } from "react-router-dom";
 import { getUser } from '../../utilities/users-service';
@@ -6,6 +5,7 @@ import AuthPage from "../AuthPage/AuthPage";
 import NewOrderPage from "../NewOrderPage/NewOrderPage";
 import OrderHistoryPage from "../OrderHistoryPage/OrderHistoryPage";
 import NavBar from "../../components/NavBar/NavBar";
+import CartDetailPage from '../CartDetailPage/CartDetailPage';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function App() {
@@ -17,16 +17,21 @@ export default function App() {
                 <>
                     <NavBar user={user} setUser={setUser}/>
                     <Switch>
-                        <Route path="/orders/new">
+                        <Route exact path="/orders/new">
                             <NewOrderPage user={user} setUser={setUser}/>
                         </Route>
-                        <Route path="/orders">
-                            <OrderHistoryPage user={user} setUser={setUser}/>
+                        <Route exact path="/orders">
+                            <OrderHistoryPage />
+                        </Route>
+                        <Route exact path="/cart">
+                            <CartDetailPage />
                         </Route>
                         <Redirect to="/orders/new"/>
                     </Switch>
                 </> 
-                : <AuthPage setUser={setUser}/>}
+                :
+                <AuthPage setUser={setUser}/>
+            }
         </main>
     );
 }
