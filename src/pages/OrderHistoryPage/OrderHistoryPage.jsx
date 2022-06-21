612bf1967a09e25debc8971ba19d19f034d1f78c
mfrancisco <francisco.melanie@gmail.com>
2022-06-21 05:58:13 -0700

    remove aside from OrderHistoryPage

--- src/pages/OrderHistoryPage/OrderHistoryPage.jsx
+++ src/pages/OrderHistoryPage/OrderHistoryPage.jsx
@@ -1,13 +1,10 @@
 import { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import Logo from '../../components/Logo/Logo';
-import UserLogOut from '../../components/UserLogOut/UserLogOut';
 import OrderList from "../../components/OrderList/OrderList";
 import OrderDetail from '../../components/OrderDetail/OrderDetail';
 import * as ordersAPI from '../../utilities/orders-api';
 import './OrderHistoryPage.css';
 
-export default function OrderHistoryPage({ user, setUser }) {
+export default function OrderHistoryPage() {
     const [orders, setOrders] = useState([]);
     const [activeOrder, setActiveOrder] = useState(null);
     
@@ -24,12 +21,6 @@ export default function OrderHistoryPage({ user, setUser }) {
 
     return (
         <main className="OrderHistoryPage">
-            {/* <h1>Order History</h1> */}
-            <aside>
-                <Logo />
-                <Link to="/orders/new" className="button btn-sm">NEW ORDER</Link>
-                <UserLogOut user={user} setUser={setUser} />
-            </aside>
             <OrderList 
                 orders={orders} 
                 setActiveOrder={setActiveOrder}
